1801c8b94fd2435c72aad22f3d9015dec3734e51
Durim Kelmendi <30774352+DurimKelmendi@users.noreply.github.com>
2023-08-21 16:02:20 +0200

    Update yahoo_api.py

--- yahoo_api.py
+++ yahoo_api.py
@@ -4,8 +4,8 @@ import json
 conn = http.client.HTTPSConnection("yahoo-finance15.p.rapidapi.com")
 
 headers = {
-    'X-RapidAPI-Key': "bdafce06f6mshe7dfa1ad7a260b3p1f23b0jsn817950867139",
-    'X-RapidAPI-Host': "yahoo-finance15.p.rapidapi.com"
+    'X-RapidAPI-Key': "PRIVATE_KEY",
+    'X-RapidAPI-Host': "PRIVATE_HOST"
     }
 
 conn.request("GET", "/api/yahoo/qu/quote/AAPL/financial-data", headers=headers)
